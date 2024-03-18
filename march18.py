620394f04f7c2104153b8a4cd07d12a8e8632baa
YourName <youremail@website.com>
2024-03-18 09:32:31 -0500

    March 18 #1

---
+++ march18.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+root= Tk()
+
+bb1=Button(root,text="Click here for nothing to happen.").grid()
+bb2=Button(root,text="Click here for nothing to happen again.").grid()
+
+root.mainloop()
