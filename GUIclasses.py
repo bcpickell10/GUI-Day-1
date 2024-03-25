56c5b2c93a0a198b06f06fe82d1e1ebc166edb9e
YourName <youremail@website.com>
2024-03-25 09:38:53 -0500

    Combining classes and GUIs

---
+++ GUIclasses.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+class GreetingApp:
+    def __init__(self, master):
+        self.label1 = ttk.Label(master)
+        self.label1.config(text="Hello")
+        self.label1.grid()
+        
+        self.btn1 = ttk.Button(master)
+        self.btn1.config(text="Press Here", command=self.goodbye_method)
+        self.btn1.grid()  # Corrected from self.btn.grid()
+
+    def goodbye_method(self):
+        print("Hello world")
+
+app = GreetingApp(root)  # Pass root to the GreetingApp constructor
+root.mainloop()
